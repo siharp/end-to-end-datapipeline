4948563a6132022f243c0bf95c26a85156b4cb83
siharp <siharpangaribuan03@gmail.com>
2026-05-12 15:04:24 +0700

    feat: add automation script for data upload and sample datasets

---
+++ data-example/upload-data-to-postgres.py
@@ -0,0 +1,41 @@
+import os
+import pandas as pd
+from sqlalchemy import create_engine, text
+
+# Koneksi ke database (Pastikan port 5438 sudah benar di mapping ke Docker)
+engine = create_engine('postgresql://sihar:sihar123@localhost:5438/olist')
+
+# --- TAMBAHAN: Pastikan schema 'staging' ada ---
+with engine.connect() as conn:
+    conn.execute(text("CREATE SCHEMA IF NOT EXISTS staging;"))
+    conn.commit()
+# ----------------------------------------------
+
+for item in os.listdir():
+    if item.endswith(".csv"):
+        try:
+            print(f"Uploading data {item} to database...")
+
+            # Membaca CSV
+            df = pd.read_csv(item)
+
+            # Cleaning nama tabel (contoh: 'orders_dataset.csv' -> 'orders')
+            table_name = item.split('.')[0].replace('_dataset', '')
+
+            # Upload menggunakan Pandas
+            df.to_sql(
+                con=engine,
+                name=table_name,
+                if_exists="replace",
+                index=False,
+                schema='staging',
+                # Menangani file besar (seperti olist_order_items)
+                chunksize=30000,
+                method='multi'   # Mempercepat proses insert
+            )
+            print(f"Success upload data '{table_name}' to schema 'staging'")
+
+        except Exception as e:
+            print(f"Failed to upload {item}. Error: {e}")
+
+print("\nSemua proses selesai!")
